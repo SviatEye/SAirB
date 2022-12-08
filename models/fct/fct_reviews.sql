0941567997980582da0bd7d6d84e4169ef701c87
Sviatoslav Stepanov <newstepsviat@gmail.com>
2022-12-08 10:50:33 +0000

    custom addons

--- models/fct/fct_reviews.sql
+++ models/fct/fct_reviews.sql
@@ -5,7 +5,9 @@
 with src_reviews as (
     select * from {{ ref("src_reviews") }}
 )
-select *
+select 
+    {{ dbt_utils.surrogate_key(['listing_id', 'review_date', 'reviewer_name', 'review_text']) }} AS review_id,
+    *
 from src_reviews
 where
     review_text is not null
